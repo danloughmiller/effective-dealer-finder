aeb4816dfdb92933e4dfa48d3b8b01a9103c75da
root <root@effect-jupiter.local>
2019-11-11 17:43:11 +0000

    Initial beta release

--- inc/filters/edealer-locationfilter.php
+++ inc/filters/edealer-locationfilter.php
@@ -37,7 +37,50 @@ class EffectiveDealer_LocationFilter extends EffectiveDealer_Filter
     
     function constructQuery(&$args, &$tax_query)
 	{
-		//if (!empty($this->currentValue))
-		//	$args['s'] = $this->currentValue;
-	}
+        if (!empty($this->currentLat) && !empty($this->currentLng)) {
+            $nearby_ids = $this->getDealersByLatLng(
+                $args['post_type'],
+                $this->currentLat,
+                $this->currentLng
+            );
+
+            $args['post__in'] = $nearby_ids;
+        }
+
+        
+    }
+    
+    protected function getDealersByLatLng($post_type, $lat, $lng, $maxDistance=100, $maxResults=3)
+    {
+        global $wpdb;
+        $sql = "SELECT wp_posts.ID, pm1.meta_value as lat, pm2.meta_value as lng,
+			3959 
+			   * acos( cos( radians($lat) ) 
+               * cos( radians(pm1.meta_value) ) 
+               * cos( radians(pm2.meta_value) - radians($lng)) + sin(radians($lat)) 
+               * sin( radians(pm1.meta_value))) as distance_miles,
+			   
+			6371 
+			   * acos( cos( radians($lat) ) 
+               * cos( radians(pm1.meta_value) ) 
+               * cos( radians(pm2.meta_value) - radians($lng)) + sin(radians($lat)) 
+               * sin( radians(pm1.meta_value))) as distance_km
+	 
+			FROM " . $wpdb->prefix. 'posts as wp_posts
+			INNER JOIN ' . $wpdb->prefix . 'postmeta as pm1 ON pm1.post_id = wp_posts.ID AND pm1.meta_key=\'dealer_latitude\' 
+			INNER JOIN ' . $wpdb->prefix . 'postmeta as pm2 ON pm2.post_id = wp_posts.ID AND pm2.meta_key=\'dealer_longitude\'
+            WHERE post_type=\'' . $post_type . '\' AND post_status=\'publish\' GROUP BY wp_posts.ID
+			ORDER BY distance_miles
+            LIMIT ' . $maxResults ;
+        
+        $results = $wpdb->get_results($sql);
+        
+        $ids = array();
+        foreach ($results as $r) {
+            if (empty($maxDistance) || $r->distance_miles < $maxDistance)
+                $ids[] = $r->ID;
+        }
+        
+        return $ids;
+    }
 }
